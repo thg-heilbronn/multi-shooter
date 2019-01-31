c580bbaf60f001af7ac75f1c4e804436ee9bf5b4
Bernd Birkicht <bir@thg.hn.bw.schule.de>
2019-01-31 15:55:36 +0100

    Anpassen der Bewegung und Gravity
    Anpassen der Maximalgeschwindigkeit und der Default-Gravity-Einstellungen des Projektes (auf 0 Gravity statt 98).

--- Ship.gd
+++ Ship.gd
@@ -5,7 +5,7 @@ extends RigidBody2D
 # var b = "text"
 var speed = 200
 var rotation_speed = 80
-var max_speed = 150
+var max_speed = 25
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,9 +17,15 @@ func _process(delta):
 	if Input.is_action_pressed("ui_up"):
 		add_force(Vector2(0,0), Vector2(cos(rotation), sin(rotation))*delta*speed)
 	
+	if Input.is_action_pressed("ui_down"):
+		add_force(Vector2(0,0), Vector2(cos(rotation), sin(rotation))*delta*(-speed))	
+	
 	if get_linear_velocity().length() > max_speed:
 		set_linear_velocity(get_linear_velocity().normalized()*max_speed)
-		
+	
+	if get_linear_velocity().length() < 0:
+		set_linear_velocity(get_linear_velocity().normalized()*0)
+	
 	if Input.is_action_pressed("ui_left"):
 		set_angular_velocity(-rotation_speed*delta)
 	
